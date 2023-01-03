dddf00de767d3078902f0c8ba90cc3fa93244e6b
Yegor Kozlov <101704236+yamara28@users.noreply.github.com>
2023-01-03 15:10:47 +0300

    feat: add solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const toPositive = Math.abs(n);
+    return Array.from(String(toPositive)).reverse().join('')
+
 }
